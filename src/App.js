1b7f190dbec512176d6dc75e2448f9c63b0e8f91
raydover <105178034+raydover@users.noreply.github.com>
2022-08-08 22:32:47 -0400

    commit about index.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
 // App.js
+// This is from 20 - modeule 24
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+const App = () => <PortfolioContainer />;
+
+export default App;
+
+
+
+
+// This is from 20 - module 23
+// import React, { useState } from 'react';
+// import Welcome from './components/Welcome';
+
+// function App() {
+//   // Here we declare a state boolean variable "loggedIn" and a function to update it.
+//   const [loggedIn, setLoggedIn] = useState(false);
+
+//   // We return the Welcome component and pass loggedIn and setLoggedIn as props.
+//   return <Welcome loggedIn={loggedIn} setLoggedIn={setLoggedIn} />;
+// }
+
+// export default App;
