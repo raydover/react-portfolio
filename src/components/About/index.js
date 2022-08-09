b9f2ab69e234ef77273ba8aa863752ba642e3177
raydover <105178034+raydover@users.noreply.github.com>
2022-08-08 23:49:44 -0400

    commit about index js paege set up

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import Avatar from '../assets/images/avatar.jpg';
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div classname='main'>
+      <h2 className='nav-heading'>About</h2>
+      <div className='about'>
+        <div>
+          <img src={Avatar} alt='Images of RD' className='avatar' />
+        </div>
+        <div>
+          <h3 className="about-heading">Hi, I'm Raymond Dover</h3>
+          <p className='about-me'>
+            Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+            Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+            mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+            lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+            imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+            in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
---
+++ src/projects.js
@@ -0,0 +1,26 @@
+import BlastOff from "./assets/images/blastoff.png";
+import EFit from "./assets/images/e-fit.png";
+
+// Projects list
+// eslint-disable-next-line import/no-anonymous-default-export
+const projects = [
+    {
+        id: 1, 
+        image: BlastOff,
+        imageAlt: "BlastOff Image",
+        title: "BlastOff",
+        liveSite: "https://blastoff2.herokuapp.com/",
+        gitHub: "https://github.com/JtheFox/blastoff"
+    },
+    {
+        id: 2, 
+        image: EFit,
+        imageAlt: "EFit Image",
+        title: "EFit",
+        liveSite: "https://polar-shelf-24197.herokuapp.com/",
+        gitHub: "https://github.com/VanessaLiaw021/E-Fit"
+    },
+];
+
+//Export projects data
+export default projects;
